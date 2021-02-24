bf3ea7d5b809030de47207c0a1926c5c0d5d1af9
Mona Lisa <clarkefraser1@gmail.com>
2021-02-24 18:58:16 +1100

    adding pseudocode, and creating basic structure

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+-- initialise with dummmy data..test data.....
+-- Eg emplyee names etc to understand how applicatioln works.
+
+-- TEST SCENARIO seed.sql. 
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
   // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'yourRootPassword'
 
   // Be sure to update with your own MySQL password!
-  password: 'yourRootPassword', // Incorrect password = Access denied error
+  password: 'leopard55', // Incorrect password = Access denied error
   database: 'employee_db', // Make sure this is an existing database
 });
 
@@ -24,3 +24,54 @@ connection.connect((err) => {
   console.log(`connected as id ${connection.threadId}`);
   connection.end();
 });
+
+
+
+const runSearch = () => {
+  inquirer
+  .prompt({
+    name: 'action',
+    type: 'rawlist',
+    message: 'What would you like to do?',
+    choices: [
+      'View Employees',
+    ],
+  })
+  .then((answer) => {
+    switch (answer.action) {
+      case 'View Employees':
+       employeeView();
+        break;
+
+      
+
+      default:
+        console.log(`Invalid action: ${answer.action}`);
+        break;
+    }
+  });
+};
+
+const employeeView = () => {
+    // inquirer
+    //   .prompt({
+    //     name: 'artist',
+    //     type: 'input',
+    //     message: 'What artist would you like to search for?',
+    //   })
+    //   .then((answer) => {
+        const query = 'SELECT id, first_name, last_name, role_id, manager_id  FROM employee '; // WHERE ? --USE FOR ADDING/SEARCHING
+        // because viewing no need for where
+        connection.query(query, {},  (err, res) => { //might be null if no results...dummy emplyees required.
+          res.forEach(({ id, first_name, last_name, role_id, manager_id }) => {
+            console.log(
+              `Position: ${id} || `// Song: ${song} || Year: ${year}`
+            );
+          });
+          runSearch();
+        });
+     // });
+  };
+  runSearch();
+
+// Finish schema file, correct syntax etc.
